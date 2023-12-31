25fcb14264d228190219d97fe6a8829f6d1561e1
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-12-31 19:40:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ const fs = require('fs').promises;
 const fileName = 'myfile.txt';
 
 const updateFile = async (fileName, fileContent) => {
-    //Write your code here to overwrite the file content
-    //Don't change function name
-    
+    try {
+        // Use fs.writeFile to update the content of the file
+        await fs.writeFile(fileName, fileContent, 'utf-8');
+        console.log(`File "${fileName}" has been updated successfully.`);
+    } catch (error) {
+        console.error(`Error updating file "${fileName}": ${error.message}`);
+    }
 };
-
 
 module.exports = updateFile;
